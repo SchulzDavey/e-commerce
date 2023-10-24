450b1d432bf530ab111cd55cbbc1a363874f2f7f
Davey Schulz <davey.schulz@hotmail.com>
2023-10-24 10:30:20 +0200

    Added stripe to get customer data

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -2,6 +2,7 @@ import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/prisma/client';
 import { NextAuthOptions } from 'next-auth';
+import Stripe from 'stripe';
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -11,6 +12,27 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+        apiVersion: '2023-10-16',
+      });
+
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+
+        await prisma.user.update({
+          where: { id: user.id },
+          data: {
+            stripeCustomerId: customer.id,
+          },
+        });
+      }
+    },
+  },
   session: {
     strategy: 'jwt',
   },
